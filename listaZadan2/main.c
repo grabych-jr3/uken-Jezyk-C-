433fe546b4e870768455e0cc61ae4bab64ee502b
Vlad Hrabovskyi <v.grabovskyi10@gmail.com>
2024-10-13 14:28:48 +0200

    Add z7

--- listaZadan2/main.c
+++ listaZadan2/main.c
@@ -5,6 +5,7 @@
 #include "z4.h"
 #include "z5.h"
 #include "z6.h"
+#include "z7.h"
 
 int main(void) {
     // z1();
@@ -12,6 +13,7 @@ int main(void) {
     // z3();
     // z4();
     // z5();
-    z6();
+    // z6();
+    z7();
     return 0;
 }
---
+++ listaZadan2/z7.h
@@ -0,0 +1,6 @@
+#ifndef Z7_H
+#define Z7_H
+
+void z7();
+
+#endif
