695704d2e976d3faf2ad57e5d5875ee40ec95028
Vlad Hrabovskyi <v.grabovskyi10@gmail.com>
2024-12-08 18:17:13 +0100

    add z5 and z6

--- listaZadan4/main.c
+++ listaZadan4/main.c
@@ -3,11 +3,14 @@
 #include "z2.h"
 #include "z3.h"
 #include "z4.h"
+#include "z5.h"
+#include "z6.h"
 
 int main(void) {
     // z1();
     // z2();
     // z3();
-    z4();
+    // z4();
+    z6();
     return 0;
 }
---
+++ listaZadan4/z6.h
@@ -0,0 +1,11 @@
+//
+// Created by vgrab on 08.12.2024.
+//
+
+#ifndef Z6_H
+#define Z6_H
+
+void wyswietl(char *c, int *num1, int *num2);
+void z6();
+
+#endif //Z6_H
