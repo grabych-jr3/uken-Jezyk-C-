5cc5fa7a6f251d73d54ecb9ba011716a3cf783e9
Vlad Hrabovskyi <v.grabovskyi10@gmail.com>
2024-12-10 20:25:24 +0100

    add z5

--- listaZadan6/main.c
+++ listaZadan6/main.c
@@ -3,11 +3,13 @@
 #include "z2.h"
 #include "z3.h"
 #include "z4.h"
+#include "z5.h"
 
 int main(void) {
     // z1();
     // z2();
     // z3();
-    z4();
+    // z4();
+    z5();
     return 0;
 }
---
+++ listaZadan6/z5.h
@@ -0,0 +1,21 @@
+//
+// Created by vgrab on 10.12.2024.
+//
+
+#ifndef Z5_H
+#define Z5_H
+
+struct punkt {
+    int x;
+    int y;
+};
+
+int f5_1(struct punkt *);
+void f5_2(struct punkt *);
+void f5_3(struct punkt *);
+int f5_4(struct punkt *, int);
+void f5_5(struct punkt *, int);
+
+void z5();
+
+#endif //Z5_H
